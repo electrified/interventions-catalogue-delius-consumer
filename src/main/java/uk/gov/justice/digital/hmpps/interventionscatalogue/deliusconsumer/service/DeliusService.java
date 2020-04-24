25196812c0d2b12628514b99be5fa8cf4f5800d5
ed <ed@maidavale.org>
2020-04-24 18:37:16 +0100

    provisional JPA mappings for provider

--- src/main/java/uk/gov/justice/digital/hmpps/interventionscatalogue/deliusconsumer/service/DeliusService.java
+++ src/main/java/uk/gov/justice/digital/hmpps/interventionscatalogue/deliusconsumer/service/DeliusService.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.entity.NsiSubType;
 import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.entity.NsiType;
+import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.entity.ProbationArea;
 import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.entity.StandardReference;
 import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.repository.NsiSubTypeRepository;
 import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.repository.NsiTypeRepository;
+import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.repository.ProbationAreaRepository;
 import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.repository.StandardReferenceRepository;
 
 import java.util.List;
@@ -17,13 +19,16 @@ public class DeliusService {
     private final NsiTypeRepository nsiTypeRepository;
     private final NsiSubTypeRepository nsiSubTypeRepository;
     private final StandardReferenceRepository standardReferenceRepository;
+    private final ProbationAreaRepository probationAreaRepository;
 
     public DeliusService(NsiTypeRepository nsiTypeRepository,
                          NsiSubTypeRepository nsiSubTypeRepository,
-                         StandardReferenceRepository standardReferenceRepository) {
+                         StandardReferenceRepository standardReferenceRepository,
+                         ProbationAreaRepository probationAreaRepository) {
         this.nsiTypeRepository = nsiTypeRepository;
         this.standardReferenceRepository = standardReferenceRepository;
         this.nsiSubTypeRepository = nsiSubTypeRepository;
+        this.probationAreaRepository = probationAreaRepository;
     }
 
     public StandardReference saveStandardReference(final StandardReference standardReference) {
@@ -40,5 +45,9 @@ public class DeliusService {
 
     public NsiSubType saveNsiSubType(final NsiSubType nsiSubType) {
         return nsiSubTypeRepository.save(nsiSubType);
+    }
+
+    public ProbationArea saveProbationArea(final ProbationArea probationArea) {
+        return probationAreaRepository.save(probationArea);
     }
 }
--- src/test/java/uk/gov/justice/digital/hmpps/interventionscatalogue/deliusconsumer/service/DeliusServiceTest.java
+++ src/test/java/uk/gov/justice/digital/hmpps/interventionscatalogue/deliusconsumer/service/DeliusServiceTest.java
@@ -5,9 +5,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.entity.Address;
 import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.entity.NsiSubType;
 import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.entity.NsiSubTypeId;
 import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.entity.NsiType;
+import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.entity.Organisation;
+import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.entity.PartitionArea;
+import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.entity.ProbationArea;
 import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.entity.ReferenceDataMaster;
 import uk.gov.justice.digital.hmpps.interventionscatalogue.deliusconsumer.jpa.entity.StandardReference;
 
@@ -83,4 +87,69 @@ class DeliusServiceTest {
 
         deliusService.saveNsiSubType(nsiSubType);
     }
+
+    @Test
+    public void saveProbationArea() {
+        var standardReference = StandardReference.builder()
+                .codeValue("test")
+                .codeDescription("test")
+                .referenceDataMaster(ReferenceDataMaster.builder()
+                        .codeSetName("test")
+                        .description("test")
+                        .listSequence('0').build())
+                .selectable('Y')
+                .createdByUserId(0)
+                .createdDateTime(new Date())
+                .lastUpdatedDateTime(new Date())
+                .lastUpdatedUserId(0)
+                .build();
+
+        var probationArea = ProbationArea.builder()
+                .code("TST")
+                .description("test")
+                .address(Address.builder()
+                        .softDeleted(0)
+                        .partitionArea(PartitionArea.builder()
+                                .area("test")
+                                .build())
+                    .createdByUserId(0)
+                    .createdDateTime(new Date())
+                    .lastUpdatedDateTime(new Date())
+                    .lastUpdatedUserId(0)
+                .build())
+                .organisation(Organisation.builder()
+                        .code("TST")
+                        .description("test")
+                        .activeFlag(1)
+                        .privateFlag(1)
+                        .startDate(new Date())
+                        .address(Address.builder()
+                                .softDeleted(0)
+                                .partitionArea(PartitionArea.builder()
+                                        .area("test")
+                                        .build())
+                                .createdByUserId(0)
+                                .createdDateTime(new Date())
+                                .lastUpdatedDateTime(new Date())
+                                .lastUpdatedUserId(0)
+                                .build())
+                        .createdByUserId(0)
+                        .createdDateTime(new Date())
+                        .lastUpdatedDateTime(new Date())
+                        .lastUpdatedUserId(0)
+                .build())
+                .privateFlag(0)
+                .probationAreaId(0)
+                .spgActiveId(standardReference)
+                .selectable('Y')
+                .startDate(new Date())
+                .createdByUserId(0)
+                .createdDateTime(new Date())
+                .lastUpdatedDateTime(new Date())
+                .lastUpdatedUserId(0)
+                .build();
+
+        deliusService.saveProbationArea(probationArea);
+    }
+
 }
